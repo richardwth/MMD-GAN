39dadf25e146eb007be17daa692ee0f9d8e0178e
richardwth <33274336+richardwth@users.noreply.github.com>
2018-10-03 14:28:23 +1000

    Update my_test_lsun.py

--- my_test_lsun.py
+++ my_test_lsun.py
@@ -52,11 +52,19 @@ code_x = np.random.randn(400, 128).astype(np.float32)
 # np.savetxt('MMD-GAN/z_128.txt', z_batch, fmt='%.6f', delimiter=',')
 # code_x = np.genfromtxt('MMD-GAN/z_128.txt', delimiter=',', dtype=np.float32)
 
-# case 1
-lr_list = [2e-4, 2e-4]  # [dis, gen]
-loss_type = 'rmb'
+# a case
+lr_list = [5e-4, 2e-4]  # [dis, gen]
+loss_type = 'rep'  
+# rep - repulsive loss, rmb - repulsive loss with bounded rbf kernel
+# to test other losses, see GeneralTools/math_func/GANLoss
+rep_weights = [0.0, -1.0]  # weights for e_kxy and -e_kyy, w[0]-w[1] must be 1
 sample_same_class = False
-sub_folder = 'sngan_{}_{:.0e}_{:.0e}_gl1_linear'.format(loss_type, lr_list[0], lr_list[1])
+if loss_type in {'rep', 'rmb'}:
+    sub_folder = 'sngan_{}_{:.0e}_{:.0e}_gl1_linear_{:.1f}_{:.1f}'.format(
+        loss_type, lr_list[0], lr_list[1], rep_weights[0], rep_weights[1])
+else:
+    sub_folder = 'sngan_{}_{:.0e}_{:.0e}_gl1_linear'.format(loss_type, lr_list[0], lr_list[1])
+# sub_folder = 'sngan_{}_{:.0e}_{:.0e}_gl1_linear'.format(loss_type, lr_list[0], lr_list[1])
 
 agent = Agent(
     filename, sub_folder, load_ckpt=True, do_trace=False,
@@ -69,38 +77,28 @@ mdl = SNGan(
     optimizer=optimizer, do_summary=True, do_summary_image=True,
     num_summary_image=8, image_transpose=False)
 
-# for i in range(1):
-#     mdl.training(
-#         filename, agent, num_instance, lr_list, end_lr=end_lr,
-#         max_step=save_per_step, batch_size=batch_size, num_threads=num_threads)
-#     if debug_mode is not None:
-#         _ = mdl.eval_sampling(
-#             filename, sub_folder, mesh_num=(20, 20), mesh_mode=0, code_x=code_x,
-#             real_sample=False, do_embedding=False, do_sprite=True)
-#     if debug_mode is not None:
-#         scores = mdl.mdl_score(
-#             filename, sub_folder, batch_size, num_batch=783, model='v1')
-#         print('Epoch {} with scores: {}'.format(i, scores))
-
-# ms-ssim
-import time
-
-start_time = time.time()
-scores = mdl.mdl_score(
-    filename, sub_folder, batch_size, num_batch=781, model='ms_ssim', ckpt_file=None)
-print('MS-SSIM scores: {}'.format(scores))
-print('\n Calculation took {:.1f} seconds'.format(time.time() - start_time))
-
-# image generation
-# iter_list = ['12500', '25000', '37500', '50000',
-#              '62500', '75000', '87500', '100000',
-#              '112500', '125000', '137500', '150000',
-#              '162500', '175000', '187500', '200000']
-# iter_list = iter_list[13:16]
-# print(iter_list)
-# for i in range(len(iter_list)):
-_ = mdl.eval_sampling(
-    filename, sub_folder, mesh_num=(6, 6), mesh_mode=0,
-    real_sample=False, do_embedding=False, do_sprite=True)
+for i in range(8):
+    mdl.training(
+        filename, agent, num_instance, lr_list, end_lr=end_lr, max_step=save_per_step,
+        batch_size=batch_size, sample_same_class=sample_same_class, num_threads=num_threads)
+    if debug_mode is not None:
+        _ = mdl.eval_sampling(
+            filename, sub_folder, mesh_num=(20, 20), mesh_mode=0, code_x=code_x,
+            real_sample=False, do_embedding=False, do_sprite=True)
+    if debug_mode is False:  # v1 - inception score and fid, ms_ssim - MS-SSIM
+        scores = mdl.mdl_score(
+            filename, sub_folder, batch_size, num_batch=781, model='v1')
+        print('Epoch {} with scores: {}'.format(i, scores))
 
 print('Chunk of code finished.')
+
+# ms-ssim
+# import time
+
+# start_time = time.time()
+# scores = mdl.mdl_score(
+#     filename, sub_folder, batch_size, num_batch=781, model='ms_ssim', ckpt_file=None)
+# print('MS-SSIM scores: {}'.format(scores))
+# print('\n Calculation took {:.1f} seconds'.format(time.time() - start_time))
+
+# print('Chunk of code finished.')
